c1d2882a8349b9d43d607edb4310630b1e60353b
Anton Nikolaenko <roxkisrover@icloud.com>
2018-09-30 01:48:07 +0500

    feat: add result template

---
+++ src/components/Result.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Result = () => (
+  <div>
+    <h1>Тип личности</h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae accusantium officia sequi modi
+      illo, nobis dolorum repellendus cumque dolores fugiat praesentium maiores, laborum dolorem
+      enim repudiandae porro, consectetur explicabo aliquam.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, quos est fugiat
+      provident, impedit pariatur corrupti ut aspernatur nam mollitia quis deleniti velit ab
+      veritatis inventore officiis eius repellat numquam!
+    </p>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias, delectus nobis
+      consequatur consequuntur totam ipsum deserunt recusandae, eligendi possimus dolores ex autem
+      voluptatem sed quasi quo, sint odit! Adipisci, minima?
+    </p>
+  </div>
+);
+
+export default Result;
