cde5830cc1e367dd3c7921b26e122e2f36944785
Anton Nikolaenko <roxkisrover@icloud.com>
2018-09-30 02:02:44 +0500

    feat: add question logic

--- src/components/AnswerButton.js
+++ src/components/AnswerButton.js
@@ -1,5 +1,40 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const AnswerButton = ({ answerText }) => <button type="button">{answerText}</button>;
+const AnswerButton = ({
+  activeAnswerIndex,
+  answerIndex,
+  answerText,
+  answerValue,
+  handleActiveIndexChange,
+  handleAnswerClick,
+  questionId,
+  questionTarget,
+}) => (
+  <button
+    type="button"
+    onClick={() => {
+      handleAnswerClick(questionId, questionTarget, answerValue);
+      handleActiveIndexChange(answerIndex);
+    }}
+  >
+    {answerText}
+  </button>
+);
+
+AnswerButton.propTypes = {
+  activeAnswerIndex: PropTypes.number,
+  answerIndex: PropTypes.number.isRequired,
+  answerText: PropTypes.string.isRequired,
+  answerValue: PropTypes.number.isRequired,
+  handleActiveIndexChange: PropTypes.func.isRequired,
+  handleAnswerClick: PropTypes.func.isRequired,
+  questionId: PropTypes.number.isRequired,
+  questionTarget: PropTypes.arrayOf(PropTypes.number).isRequired,
+};
+
+AnswerButton.defaultProps = {
+  activeAnswerIndex: null,
+};
 
 export default AnswerButton;
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import AnswerButton from './AnswerButton';
 
 class Question extends React.Component {
@@ -15,16 +16,40 @@ class Question extends React.Component {
   }
 
   render() {
+    const { activeAnswerIndex } = this.state;
+    const {
+      answersData, handleAnswerClick, questionId, questionTarget, questionText,
+    } = this.props;
     return (
       <div>
-        <p>Question</p>
-        <AnswerButton />
-        <AnswerButton />
-        <AnswerButton />
+        <p>
+          {questionId}. {questionText}
+        </p>
+        {answersData.map((answer, index) => (
+          <AnswerButton
+            activeAnswerIndex={activeAnswerIndex}
+            answerIndex={index}
+            answerText={answer.text}
+            answerValue={answer.value}
+            handleActiveIndexChange={this.handleActiveIndexChange}
+            handleAnswerClick={handleAnswerClick}
+            key={answer.id}
+            questionId={questionId}
+            questionTarget={questionTarget}
+          />
+        ))}
         <hr />
       </div>
     );
   }
 }
+
+Question.propTypes = {
+  answersData: PropTypes.arrayOf(PropTypes.object).isRequired,
+  handleAnswerClick: PropTypes.func.isRequired,
+  questionId: PropTypes.number.isRequired,
+  questionTarget: PropTypes.arrayOf(PropTypes.number).isRequired,
+  questionText: PropTypes.string.isRequired,
+};
 
 export default Question;
