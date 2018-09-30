b99274ab7d39e4bac6a0e368e604670185c3166f
Anton Nikolaenko <roxkisrover@icloud.com>
2018-09-30 14:09:57 +0500

    feat: add styled-components

--- src/components/AnswerButton.js
+++ src/components/AnswerButton.js
@@ -1,8 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const isActive = props => props.answerIndex === props.activeAnswerIndex;
+
+const Button = styled.button`
+  display: inline-block;
+  margin-right: 10px;
+  margin-bottom: 10px;
+  height: 28px;
+  padding: 0 30px;
+  color: ${isActive ? '#fff' : '#555'};
+  text-align: center;
+  font-size: 11px;
+  font-weight: 600;
+  line-height: 28px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  text-decoration: none;
+  white-space: nowrap;
+  background-color: ${isActive ? '#33c3f0' : 'transparent'};
+  border-radius: 4px;
+  border: ${isActive ? '1px solid #33c3f0' : '1px solid #bbb'};
+  cursor: pointer;
+  box-sizing: border-box;
+
+  :hover,
+  :focus {
+    color: ${isActive ? '#fff' : '#333'};
+    border-color: ${isActive ? '#1eaedb' : '#888'};
+    background-color: ${isActive ? '#1eaedb' : 'transparent'};
+    outline: 0;
+  }
+`;
 
 const AnswerButton = ({
-  activeAnswerIndex,
+  // activeAnswerIndex,
   answerIndex,
   answerText,
   answerValue,
@@ -11,7 +44,7 @@ const AnswerButton = ({
   questionId,
   questionTarget,
 }) => (
-  <button
+  <Button
     type="button"
     onClick={() => {
       handleAnswerClick(questionId, questionTarget, answerValue);
@@ -19,7 +52,7 @@ const AnswerButton = ({
     }}
   >
     {answerText}
-  </button>
+  </Button>
 );
 
 AnswerButton.propTypes = {
