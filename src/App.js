d3ca38e884aff82f5072c12f9f1b3650a1379178
Anton Nikolaenko <roxkisrover@icloud.com>
2018-09-30 13:21:53 +0500

    fix: improve routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Frontpage from './components/Frontpage';
 import OldhamMorris from './components/OldhamMorris';
 import Result from './components/Result';
+import Error from './components/Error';
 
 export default () => (
-  <div>
+  <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={Frontpage} />
-      <Route exact path="/oldham-morris" component={OldhamMorris} />
-      <Route path="/oldham-morris/:type" component={Result} />
+      <Route path="/" component={Frontpage} exact />
+      <Route path="/oldham-morris" component={OldhamMorris} exact />
+      <Route path="/oldham-morris/type" component={Result} exact />
+      <Route component={Error} />
     </Switch>
-  </div>
+  </BrowserRouter>
 );
---
+++ src/components/Error.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Error = () => <p>Error: path does not exist.</p>;
+
+export default Error;
