4b2d7842bdd18b0a2501254befbaed285919948a
Anton Nikolaenko <roxkisrover@icloud.com>
2018-10-24 21:56:00 +0500

    chore: update service worker

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import 'normalize.css';
 import './index.css';
 import App from './App';
 import rootReducer from './reducers';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer);
 
@@ -16,4 +16,8 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
