6ae447ff457af718fb15f3eab9e9cd350178c211
Anton Nikolaenko <roxkisrover@icloud.com>
2018-11-05 22:43:50 +0500

    feat: integrate ant design

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import 'normalize.css';
-import './index.css';
-import App from './App';
 import rootReducer from './reducers';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer);
