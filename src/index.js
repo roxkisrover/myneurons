05423c742e47d8efc3ad9a1c49179424dbe23a89
Anton Nikolaenko <roxkisrover@icloud.com>
2018-12-02 21:42:55 +0500

    chore: remove service worker

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { LocaleProvider } from 'antd';
 import ruRU from 'antd/lib/locale-provider/ru_RU';
 import rootReducer from './reducers';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const store = createStore(rootReducer);
 
@@ -18,8 +17,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root'),
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
