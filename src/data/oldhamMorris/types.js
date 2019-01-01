b602ae14505fb79789bb4d84f3b48bf24131a9d8
Anton Nikolaenko <roxkisrover@icloud.com>
2019-01-01 22:54:16 +0500

    feat: new om test types

--- src/data/oldhamMorris/types.js
+++ src/data/oldhamMorris/types.js
@@ -6,68 +6,48 @@ const omTypesData = [
   },
   {
     id: 2,
-    title: 'Одинокий',
-    link: '/solitary',
-  },
-  {
-    id: 3,
     title: 'Идеосинкратический',
     link: '/idiosyncratic',
   },
   {
-    id: 4,
-    title: 'Авантюрный',
-    link: '/adventurous',
-  },
-  {
-    id: 5,
+    id: 3,
     title: 'Деятельный',
     link: '/mercurial',
   },
   {
-    id: 6,
-    title: 'Драматичный',
-    link: '/dramatic',
+    id: 4,
+    title: 'Одинокий',
+    link: '/solitary',
   },
   {
-    id: 7,
-    title: 'Самоуверенный',
-    link: '/self-confident',
-  },
-  {
-    id: 8,
-    title: 'Чувствительный',
-    link: '/sensitive',
-  },
-  {
-    id: 9,
+    id: 5,
     title: 'Преданный',
     link: '/devoted',
   },
   {
-    id: 10,
+    id: 6,
+    title: 'Самоуверенный',
+    link: '/self-confident',
+  },
+  {
+    id: 7,
+    title: 'Драматичный',
+    link: '/dramatic',
+  },
+  {
+    id: 8,
     title: 'Добросовестный',
     link: '/conscientious',
   },
   {
-    id: 11,
-    title: 'Праздный',
-    link: '/leisurely',
+    id: 9,
+    title: 'Чувствительный',
+    link: '/sensitive',
   },
   {
-    id: 12,
-    title: 'Агрессивный',
-    link: '/aggressive',
-  },
-  {
-    id: 13,
-    title: 'Альтруистический',
-    link: '/self-sacrificing',
-  },
-  {
-    id: 14,
-    title: 'Серьёзный',
-    link: '/serious',
+    id: 10,
+    title: 'Авантюрный',
+    link: '/adventurous',
   },
 ];
 
