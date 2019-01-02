4883c1a9a6962e38d0fd6804f6afe6cd01ed0846
Anton Nikolaenko <roxkisrover@icloud.com>
2019-01-02 22:47:01 +0500

    feat: add redux-logger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
 import { LocaleProvider } from 'antd';
 import ruRU from 'antd/lib/locale-provider/ru_RU';
 import rootReducer from './reducers';
 import App from './components/App';
 
-const store = createStore(rootReducer);
+const middleware = [];
+const logger = createLogger({ collapsed: true });
+
+if (process.env.ENVIRONMENT !== 'production') {
+  middleware.push(logger);
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middleware));
 
 ReactDOM.render(
   <Provider store={store}>
