af231e5d8b57fa12e9a0e48776ed5e7b5e277d99
Anton Nikolaenko <roxkisrover@icloud.com>
2019-01-03 21:54:31 +0500

    feat: add tokens

--- src/data/oldhamMorris/types.js
+++ src/data/oldhamMorris/types.js
@@ -2,56 +2,67 @@ const omTypesData = [
   {
     id: 0,
     title: 'Недостаточно правдивый',
+    token: 'falsified',
     link: '/falsified',
   },
   {
     id: 1,
     title: 'Бдительный',
+    token: 'vigilant',
     link: '/vigilant',
   },
   {
     id: 2,
     title: 'Идеосинкратический',
+    token: 'idiosyncratic',
     link: '/idiosyncratic',
   },
   {
     id: 3,
     title: 'Деятельный',
+    token: 'mercurial',
     link: '/mercurial',
   },
   {
     id: 4,
     title: 'Одинокий',
+    token: 'solitary',
     link: '/solitary',
   },
   {
     id: 5,
     title: 'Преданный',
+    token: 'devoted',
     link: '/devoted',
   },
   {
     id: 6,
     title: 'Самоуверенный',
+    token: 'self-confident',
     link: '/self-confident',
   },
   {
     id: 7,
     title: 'Драматичный',
+    token: 'dramatic',
     link: '/dramatic',
   },
   {
     id: 8,
     title: 'Добросовестный',
+    token: 'conscientious',
     link: '/conscientious',
   },
   {
     id: 9,
     title: 'Чувствительный',
+    token: 'sensitive',
     link: '/sensitive',
   },
   {
     id: 10,
     title: 'Авантюрный',
+    token: 'adventurous',
     link: '/adventurous',
   },
 ];
