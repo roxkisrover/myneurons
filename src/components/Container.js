6abcf7ac591ff151630355c11eaa3fda19abc597
Anton Nikolaenko <roxkisrover@icloud.com>
2019-01-08 21:13:13 +0500

    feat: add container shadow when vertical centering applied

--- src/components/Container.js
+++ src/components/Container.js
@@ -19,11 +19,14 @@ const Content = styled.div`
   max-width: 960px;
   border-radius: 10px;
   background-color: #fff;
+  ${props => (props.centered
+    ? 'box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)'
+    : '')};
 `;
 
 const Container = ({ bgColored, children, isVerticallyCentered }) => (
   <Wrapper colored={bgColored} centered={isVerticallyCentered}>
-    <Content>{children}</Content>
+    <Content centered={isVerticallyCentered}>{children}</Content>
   </Wrapper>
 );
 
